6f20644126b4edde5e17fe1b8d578d87c48c6998
kushagraparmar67 <kushagraparmar1228@gmail.com>
2025-01-24 11:47:36 +0530

    Deploy_by_CMD

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
 
     return (
+        <>
+        <Header/>
         <div className='App'>
             <h1>Vite + Reactooooo by kushagra</h1>
         </div>
+        </>
     );
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      Hello this is Header 
+    </header>
+  )
+}
+
+export default Header
